351949cde6e92cc68183096a8a15ecb704c188fb
AmmarSyedK <76450681+AmmarSyedK@users.noreply.github.com>
2023-02-12 17:10:34 +0100

    Adding CORS

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const data = require('./data.json')
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = 8080;
 
@@ -8,7 +9,7 @@ app.listen(
     () => console.log(`it's alive on http://localhost:${PORT}`)
 );
 
-app.use(express.json());
+app.use(cors());
 
 app.get('/recipes', (req, res) => {
     res.status(200).send(data)
